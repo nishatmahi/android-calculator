b8c62c501b4a677dcda4ca1614dda832bd6dc5a0
nishatmahi <mahitasnim03@gmail.com>
2023-03-21 16:53:41 +0600

    Add JavaClass1

---
+++ app/src/main/java/zone/eloy/projects/androidcalculator/JavaClass1.java
@@ -0,0 +1,30 @@
+package zone.eloy.projects.androidcalculator;
+
+public class JavaClass1 {
+    String stud_name;
+    int id;
+
+    public JavaClass1() {
+    }
+
+    public JavaClass1(String stud_name, int id) {
+        this.stud_name = stud_name;
+        this.id = id;
+    }
+
+    public String getStud_name() {
+        return stud_name;
+    }
+
+    public void setStud_name(String stud_name) {
+        this.stud_name = stud_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
