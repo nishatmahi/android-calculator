a5ba41641a153ae2bd53d07a9f61f417e7638ece
nishatmahi <mahitasnim03@gmail.com>
2023-03-21 17:10:53 +0600

    MOdified Javaclass1 again

--- app/src/main/java/zone/eloy/projects/androidcalculator/JavaClass1.java
+++ app/src/main/java/zone/eloy/projects/androidcalculator/JavaClass1.java
@@ -24,7 +24,7 @@ public class JavaClass1 {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setInt(){
+        System.out.println("Changed again");
     }
 }
